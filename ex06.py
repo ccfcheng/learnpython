ef4da415b3a3d50fb219c34c159aa076374c843d
Cliff Saporta Cheng <ccfcheng@gmail.com>
2017-01-12 16:44:17 -0800

    - Finished exercise 6

---
+++ ex06.py
@@ -0,0 +1,9 @@
+a = "Python string interpolation example in exercise %d" % 6
+b = 'Cliff %s %s' % ('Saporta', 'Cheng')
+c = "Using a string in another: %s" % b
+d = "Raw data interpolation: %r" % b
+
+print(a)
+print(b)
+print(c)
+print(d)
